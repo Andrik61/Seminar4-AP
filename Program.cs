6a1c985fabe566943a1cfec3f65873a12d6a4d4f
Andrik61 <andrey@mail.ru>
2022-08-18 19:58:44 +0300

    Seminar 4

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.WriteLine("Введите число A: " );
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B: " );
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// double result = Math.Pow(numberA, numberB);
+// Console.WriteLine(result);
+
+// 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+//  Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int num = number.Length;
+// Console.WriteLine($"Сумма цифр в числе: {num[0,1,N]}");
+
+
+
+// 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
